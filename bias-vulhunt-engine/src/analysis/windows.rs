1b527c39038e91d3f4246fcabeaab015a865be82
prescigno <paolo.rescigno@studenti.unimi.it>
2026-05-04 11:38:49 +0000

    Aggiunta analizzatore per windows

--- bias-vulhunt-engine/src/analysis/windows.rs
+++ bias-vulhunt-engine/src/analysis/windows.rs
@@ -16,6 +16,7 @@ use bias::pipeline::PipelineError;
 use bias::platform::common::flirt::FLIRTSymbolManager;
 use bias::platform::common::types::{TypeManager, TypeManagerError};
 use bias::platform::windows::WindowsBinary;
+use bias::platform::windows::WindowsBinaryAnalysis;
 use bias::platform::{PlatformAttributes, PlatformProvider};
 use bias::reporting::{CodeReportBuilder, CodeReportRenderer};
 
